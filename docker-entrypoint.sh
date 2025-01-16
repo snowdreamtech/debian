daf76354167b9c58f9114339f9a438a64c0b9f77
snowdream <sn0wdr1am@qq.com>
2025-01-16 19:40:06 +0800

    format: docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,7 @@ fi
 # https://github.com/docker/compose/issues/1926#issuecomment-422351028
 if [ "${KEEPALIVE}" -eq 1 ]; then
     trap : TERM INT
-    tail -f /dev/null & wait
+    tail -f /dev/null &
+    wait
     # sleep infinity & wait
 fi
